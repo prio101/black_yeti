6c2aa9e43b21a563da16be87f90bbd249d07543e
Mahabub Islam Prio <miprio101@gmail.com>
2022-10-06 21:33:23 +0600

    updated docker file to byebug
    - added tty output to docker
    - added the endpoints fully

--- black_yeti/app/controllers/api/v1/services_controller.rb
+++ black_yeti/app/controllers/api/v1/services_controller.rb
@@ -3,7 +3,7 @@
 module Api
   module V1
     class ServicesController < Api::ApiController
-      before_action :set_serivce, only: %i[:show, :delete, :update]
+      before_action :set_serivce, only: [:show, :delete, :update]
 
       def index
         collection = Service.page(params[:page]).per((params[:per_page] || Service::DEFAULTS_PER_PAGE).to_i)
@@ -26,34 +26,35 @@ module Api
         if service.save!
           render json: service, status: :created
         else
-          # check and test
           render json: error_saving_data(service.errors), status: 401
         end
       end
 
       def update
-        service = Service.find_by(name: service_params['name'])
-        service.name = service_params['name']
-        service.tag = service_params['tag']
-        service.description = service_params['description']
-        service.published = service_params['published']
-        if service.save!
+        @service.name = service_params['name']
+        @service.description = service_params['description']
+        @service.tag = service_params['tag']
+        @service.published = service_params['published']
+
+        if @service.save!
           render json: @service , status: :ok
+        else
+          render json: data_operation_not_success, status: 404
         end
       end
 
       def delete
         if @service.delete
-          render json: { data: 'Record Deleted Successfully' }, status: :ok
+          render json: data_deleted, status: :ok
         else
-          render json: { data: 'Record Could not be deleted' }, status: 401
+          render json: data_operation_not_success, status: 401
         end
       end
 
       private
 
       def set_serivce
-        @service = Service.find_by(id: params[:id])
+        @service = Service.find(params[:id])
       end
 
       def service_params
--- black_yeti/app/controllers/concerns/api_messages.rb
+++ black_yeti/app/controllers/concerns/api_messages.rb
@@ -6,4 +6,12 @@ module ApiMessages
   def error_saving_data(e)
     { data: 'Error Saving the data set', errors: e.messages }
   end
+
+  def data_deleted
+    { data: 'Record Deleted Successfully' }
+  end
+
+  def data_operation_not_success
+    { data: 'Record Could not be deleted' }
+  end
 end
